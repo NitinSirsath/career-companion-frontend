e21198b831fe9df2a299598c65a5f947444ec82c
Nitin Sirsath <nitinsirsath8855@gmail.com>
2026-09-23 01:01:05 +0530

    chore: remove scripts

--- fix_gmail.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/tests/gmail.test.tsx', 'utf8');
-content = content.replace(/total: 0,\n      limit: 50,\n      offset: 0/g, "metadata: { limit: 50, offset: 0, nextOffset: null }");
-fs.writeFileSync('src/tests/gmail.test.tsx', content, 'utf8');
